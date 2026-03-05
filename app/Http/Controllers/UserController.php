d14f9757913982b0a98ec656dc9b3d816f79eeb0
san_wyd <kurniawansandy808@gmail.com>
2026-03-05 14:54:12 +0700

    jobsheet04 P2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,10 @@ class UserController extends Controller
 {
     public function index()
 {   
-    $user = UserModel::where('username', 'manager9')   ->firstOrFail();
+    $user = UserModel::where('level_id', 2)-> count();
+    //dd($user);
+
+
     // Kirim ke view
     return view('user', ['data' => $user]);
 }

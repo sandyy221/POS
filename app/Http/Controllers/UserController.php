50b7c21aa568c1e60becd6be68639fb1ae81c4ea
san_wyd <kurniawansandy808@gmail.com>
2026-03-05 18:21:11 +0700

    jobsheet04 P2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,15 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
+    /*public function index()
     {
         $user = UserModel::all();
 
+        return view('user', ['data' => $user]);
+    }*/
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
     public function tambah()

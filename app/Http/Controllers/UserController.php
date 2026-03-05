b4478b50aec086143f0629b4b6b6ee5311419705
san_wyd <kurniawansandy808@gmail.com>
2026-03-05 15:23:23 +0700

    jobsheet04 P2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,13 @@ class UserController extends Controller
 {
     public function index()
 {   
-    $user = UserModel::where('level_id', 2)-> count();
-    //dd($user);
-
+    $user = UserModel::firstOrNew(
+        ['username' =>'manager33',
+        'nama'=>'Manager Tiga Tiga',
+        'password'=> Hash::make('12345'),
+        'level_id'=>2]
+    );
+    $user->save();
 
     // Kirim ke view
     return view('user', ['data' => $user]);

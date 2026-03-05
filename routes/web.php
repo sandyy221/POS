b46ad376731ea4c92ab776cba49f2071053a1a5f
san_wyd <kurniawansandy808@gmail.com>
2026-03-05 17:39:56 +0700

    jobsheet04 P2.6

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,8 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 Route::get('/Level', [LevelController::class, 'index']);
 Route::get('/Kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);

5d3705a9c05371879f5535b902b69b9916b63fce
san_wyd <kurniawansandy808@gmail.com>
2026-03-05 13:52:17 +0700

    josbheet04 P1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'hai',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 5
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::insert($data); // tambahkan data ke tabel m_user
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user

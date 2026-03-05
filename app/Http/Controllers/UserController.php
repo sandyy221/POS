0885e1817d530ec04e407f5cd8efa6fe829a90d7
san_wyd <kurniawansandy808@gmail.com>
2026-03-05 14:30:12 +0700

    jobsheet04 P2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,17 +9,12 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-    {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
-    }
+{   
+    $user = UserModel::findOr(20,['username', 'nama'], function(){
+        abort(404);
+    });
+    
+    // Kirim ke view
+    return view('user', ['data' => $user]);
+}
 }
